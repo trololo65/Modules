b6990b2b22cb7f7185400ef49a0a4abaef83c465
trololo65 <82900214+trololo65@users.noreply.github.com>
2022-02-14 22:53:21 +0500

    Add files via upload

--- Num.py
+++ Num.py
@@ -1,5 +1,5 @@
 from .. import loader, utils
-import asyncio, pytz
+import asyncio, pytz, re
 from telethon.tl.types import MessageEntityTextUrl
 import json as JSON
 from datetime import datetime, date, time
@@ -202,3 +202,73 @@ class NumMod(loader.Module):
 				infList[user] = [str(count), vremya]
 				self.db.set("NumMod", "infList", infList)
 				await utils.answer(message, f"Пользователь <code>{user}</code> добавлен в список заражений.\nЧисло: <code>{count}</code>\nДата: <b>{vremya}</b>")
+
+	async def numfiltercmd(self, message):
+		""" .numfilter {args1} {args2 OR reply} \nВызови команду, чтобы просмотреть аргументы."""
+		args = utils.get_args_raw(message)
+		reply = await message.get_reply_message()
+		filter_and_users = self.db.get("NumMod", "numfilter", {'users': [], 'filter': None, 'status': False})
+		if not args:
+			return await utils.answer(message, f"\
+				-sU --- добавить|удалить юзеров(не больше 5), на которых будет триггериться фильтр(ид|реплай).\n\
+[{', '.join(list('<code>' + i + '</code>' for i in filter_and_users['users']))}]\n\
+-sF --- установить фильтр. Допустим один.\n\
+<code>{filter_and_users['filter'] if filter_and_users['filter'] else '❌Не установлен.'}</code>\n\
+-t --- запустить|остановить.\n\
+<b>{'✅Запущен' if filter_and_users['status'] else '❌Остановлен'}.</b>\n\n\
+Работает так:\n\
+[фильтр] зарази(ть) (1-10) @id|user\n\
+[фильтр] лечись|вакцин[ау]|купи[ть] вакцину\n\
+Игнор регистра!!")
+		args = args.split(' ', maxsplit=1)
+		if len(args) == 1 and not reply and args[0] != '-t':
+			return await utils.answer(message, '❌ Нет 2 аргумента и реплая.')
+		elif args[0] == '-sU':
+			try:
+				user_id = args[1]
+				if not user_id.isdigit():
+					return await utils.answer(message, 'Это не ид.')
+			except:
+				user_id = str(reply.sender_id)
+			if user_id in filter_and_users['users']:
+				filter_and_users['users'].remove(user_id)
+				await utils.answer(message, f"✅ Ид <code>{user_id}</code> удалён.")
+			elif len(filter_and_users['users']) <= 5:
+				filter_and_users['users'].append(user_id)
+				await utils.answer(message, f"✅ Ид <code>{user_id}</code> добавлен.")
+			else:
+				return await utils.answer(message, '❌ Превышен лимит в 5 юзеров.')
+			return self.db.set("NumMod", "numfilter", filter_and_users)
+		elif args[0] == '-sF':
+			try:
+				filter_and_users['filter'] = args[1].lower().strip()
+				self.db.set("NumMod", "numfilter", filter_and_users)
+				return await utils.answer(message, f"✅ Фильтр ~~~ <code>{args[1]}</code> ~~~ успешно установлен!")
+			except:
+				return await utils.answer(message, "Где 2 аргумент❓")
+		elif args[0] == '-t':
+			if filter_and_users['status']:
+				filter_and_users['status'] = False
+				self.db.set("NumMod", "numfilter", filter_and_users)
+				return await utils.answer(message, "❌ Фильтр остановлен.")
+			else:
+				filter_and_users['status'] = True
+				self.db.set("NumMod", "numfilter", filter_and_users)
+				return await utils.answer(message, "✅ Фильтр запущен.")
+		else:
+			return await utils.answer(message, "❌ Неизвестный аргумент.")
+
+	async def watcher(self, message):
+		filter_and_users = self.db.get("NumMod", "numfilter", {'users': [], 'filter': None, 'status': False})
+		user_id = str(message.sender_id)
+		if not filter_and_users['filter'] or not filter_and_users['status'] or user_id not in filter_and_users['users'] or message.is_private: return
+		text = message.raw_text.lower()
+		if not text.startswith(filter_and_users['filter']): return
+		send_mes = re.search(r"зарази[ть]{,2}(\s[1-9]?[0]?)?\s@[1-9a-z]+", text)
+		if send_mes:
+			send_mes = send_mes.string[send_mes.start():send_mes.end()].split(' ', maxsplit=1)
+			send_mes[0] = 'заразить'
+			await message.respond(' '.join(send_mes))
+		else:
+			send_mes = re.search(r"лечись|вакцин[ау]|купи[ть]{,2} вакцину", text).string
+			await message.respond('.купить вакцину')
